06e9d2b0830b1ff7e1127b721cafc3507fc12d95
Johnny Lee <johnnylee@Johnnys-Air-2.cscscience.ca>
2025-05-26 14:01:20 -0500

    Cleaned up Linux Warnings

--- src/losses/CrossEntropy.cpp
+++ src/losses/CrossEntropy.cpp
@@ -11,12 +11,12 @@ double CrossEntropy::calculateLoss(int label, const vector<double> &probabilitie
 
 vector<double> CrossEntropy::calculateGradient(int label, const vector<double> &activations) const {
     size_t size = activations.size();
-    size_t label = (size_t) label;
+    size_t labelIdx = (size_t) label;
     vector<double> gradient(size, 0.0);
     
     #pragma omp parallel for
     for (size_t i = 0; i < size; i++) {
-        if (i == label) {
+        if (i == labelIdx) {
             gradient[i] = TrainingUtils::clipDerivative(activations[i] - 1);
         } else {
             gradient[i] = TrainingUtils::clipDerivative(activations[i]);
