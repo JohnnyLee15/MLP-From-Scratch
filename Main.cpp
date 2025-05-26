0679cc0729ac7493d03aec87d75db10d0cb2a529
Johnny Lee <johnnylee@Johnnys-Air-2.cscscience.ca>
2025-05-26 10:22:09 -0500

    Test using PAT

--- Main.cpp
+++ Main.cpp
@@ -22,6 +22,7 @@ int main() {
     vector<int> layerSizes = {784, 32, 16, 10};
 
     NeuralNet nn(layerSizes, activations, loss);
+
     nn.train(reader, 0.01, 0.1, 20, 32);
     double accuracy = nn.test(reader.getTestFeatures(), reader.getTestTarget());
 
