9d6e46494c0696fe7cb6dce300576065d1561e28
Johnny Lee <johnnylee@Johnnys-MacBook-Air-2.local>
2025-05-29 02:09:44 -0500

    Added more OpenMP

--- src/utils/Matrix.cpp
+++ src/utils/Matrix.cpp
@@ -16,7 +16,7 @@ Matrix::Matrix(const vector<vector<double> > &mat) : numRows(mat.size()) {
 
     matrix = vector<double>(numRows * numCols);
 
-    #pragma omp parallel for
+    #pragma omp parallel for collapse(2)
     for (int i = 0; i < numRows; i++) {
         for (int j = 0; j < numCols; j++) {
             matrix[i * numCols + j] = mat[i][j];
@@ -168,7 +168,7 @@ Matrix& Matrix::operator +=(const Matrix &mat2) {
 void Matrix::addToRows(const vector<double> &vec) {
     assert(vec.size() == numCols);
 
-    #pragma omp parallel for
+    #pragma omp parallel for collapse(2)
     for (size_t i = 0; i < numRows; i++) {
         for (size_t j = 0; j < numCols; j++) {
             matrix[i * numCols + j] += vec[j];
