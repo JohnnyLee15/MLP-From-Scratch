4c6d6f84fe5692cd0814284063c82376c363714f
Johnny Lee <johnnylee@Johnnys-MacBook-Air-2.local>
2025-08-16 09:11:16 -0500

    Cleaning mains

--- src/mains/CNNTrain.cpp
+++ src/mains/CNNTrain.cpp
@@ -1,123 +1,139 @@
-// #include <fstream>
-// #include <iostream>
-// #include <vector>
-// #include <sstream>
-// #include "core/model/NeuralNet.h"
-// #include "core/data/TabularData.h"
-// #include <iomanip>
-// #include "core/activations/ReLU.h"
-// #include "core/activations/Softmax.h"
-// #include "core/activations/Linear.h"
-// #include "core/losses/MSE.h"
-// #include "core/losses/Loss.h"
-// #include "core/losses/SoftmaxCrossEntropy.h"
-// #include "utils/ConsoleUtils.h"
-// #include "utils/Greyscale.h"
-// #include "utils/Minmax.h"
-// #include "utils/TrainingUtils.h"
-// #include "core/layers/Layer.h"
-// #include "core/layers/Dense.h"
-// #include "core/metrics/ProgressAccuracy.h"
-// #include "core/metrics/ProgressMAPE.h"
-// #include "core/model/Pipeline.h"
-// #include "utils/ImageTransform2D.h"
-// #include "core/layers/Conv2D.h"
-// #include "core/layers/MaxPooling2D.h"
-// #include "core/layers/Flatten.h"
-// #include "core/layers/GlobalAveragePooling2D.h"
-// #include "core/layers/Dropout.h"
-// #include "core/data/ImageData2D.h"
-// #include "core/gpu/GpuEngine.h"
-// #include "utils/EarlyStop.h"
-// #include "utils/DataSplitter.h"
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include "core/model/NeuralNet.h"
+#include "core/data/TabularData.h"
+#include <iomanip>
+#include "core/activations/ReLU.h"
+#include "core/activations/Softmax.h"
+#include "core/activations/Linear.h"
+#include "core/losses/MSE.h"
+#include "core/losses/Loss.h"
+#include "core/losses/SoftmaxCrossEntropy.h"
+#include "utils/ConsoleUtils.h"
+#include "utils/Greyscale.h"
+#include "utils/Minmax.h"
+#include "utils/TrainingUtils.h"
+#include "core/layers/Layer.h"
+#include "core/layers/Dense.h"
+#include "core/metrics/ProgressAccuracy.h"
+#include "core/metrics/ProgressMAPE.h"
+#include "core/model/Pipeline.h"
+#include "utils/ImageTransform2D.h"
+#include "core/layers/Conv2D.h"
+#include "core/layers/MaxPooling2D.h"
+#include "core/layers/Flatten.h"
+#include "core/layers/GlobalAveragePooling2D.h"
+#include "core/layers/Dropout.h"
+#include "core/data/ImageData2D.h"
+#include "core/gpu/GpuEngine.h"
+#include "utils/EarlyStop.h"
+#include "utils/DataSplitter.h"
 
-// int main() {
-//     // Welcome Message
-//     ConsoleUtils::printTitle();
+int main() {
+    // Welcome Message
+    ConsoleUtils::printTitle();
 
-//     // Initialize Gpu if on Mac (Safe to call on non-Mac, it just won't do anything)
-//     GpuEngine::init();
+    // Initialize Gpu if on Mac (Safe to call on non-Mac, it just won't do anything)
+    GpuEngine::init();
 
-//     // Image Resize Dims
-//     const size_t SIZE = 28;
+    // Image Resize Dims
+    const size_t SIZE = 224;
 
-//     // Number of channels to read in
-//     const size_t CHANNELS = 1;
+    // Number of channels to read in
+    const size_t CHANNELS = 1;
 
-//     // Data Paths
-//     const string trainPath = "DataFiles/mnist_png/train";
-//     const string testPath = "DataFiles/mnist_png/test";
+    // Data Paths
+    const string dataPath = "DataFiles/chest_xray/data";
 
-//     // Data Reading
-//     ImageData2D *data = new ImageData2D();
-//     data->readTrain(trainPath, CHANNELS);
-//     data->readTest(testPath, CHANNELS);
+    // Data Reading
+    ImageData2D *data = new ImageData2D();
+    data->readTrain(dataPath, CHANNELS);
 
-//     // Transform data (resize to 224x224 and normalize)
-//     ImageTransform2D *transformer = new ImageTransform2D(SIZE, SIZE, CHANNELS);
-//     Tensor x = transformer->transform(data->getTrainFeatures());
-//     Tensor xTest = transformer->transform(data->getTestFeatures());
+    // Transform data (resize to 224x224 and normalize)
+    ImageTransform2D *transformer = new ImageTransform2D(SIZE, SIZE, CHANNELS);
+    Tensor x = transformer->transform(data->getTrainFeatures());
+    vector<float> y = data->getTrainTargets();
 
-//     vector<float> y= data->getTrainTargets();
-//     vector<float> yTest = data->getTestTargets();
+    // Splitting Data into training and testing set
+    Split splitTest = DataSplitter::stratifiedSplit(x, y, 0.2f);
+    const Tensor &xTest = splitTest.xVal;
+    const vector<float> &yTest = splitTest.yVal;
 
-//     // Splitting training data into train and validation sets
-//     Split split = DataSplitter::stratifiedSplit(x, y, 0.1f);
-//     const Tensor &xTrain = split.xTrain;
-//     const Tensor &xVal = split.xVal;
-//     const vector<float> &yTrain = split.yTrain;
-//     const vector<float> &yVal = split.yVal;
-    
+    // Splitting training set into train and validation set
+    Split splitVal = DataSplitter::stratifiedSplit(splitTest.xTrain, splitTest.yTrain, 0.1f);
+    const Tensor &xTrain = splitVal.xTrain;
+    const Tensor &xVal = splitVal.xVal;
+    const vector<float> &yTrain= splitVal.yTrain;
+    const vector<float> &yVal = splitVal.yVal;
 
-//     // Defining Model Architecture
-//     Loss *loss = new SoftmaxCrossEntropy();
-//     vector<Layer*> layers = {
-//         new Conv2D(64, 3, 3, 1, "same", new ReLU()),  
-//         new MaxPooling2D(2, 2, 2, "none"), 
-        
-//         new Conv2D(128, 3, 3, 1, "same", new ReLU()),
-//         new Conv2D(128, 3, 3, 1, "same", new ReLU()),
-//         new MaxPooling2D(2, 2, 2, "none"), 
+    // Defining Model Architecture
+    Loss *loss = new SoftmaxCrossEntropy();
+    vector<Layer*> layers = {
+        new Conv2D(32, 3, 3, 1, "same", new ReLU(), 5e-4f),
+        new Conv2D(32, 3, 3, 1, "same", new ReLU(), 5e-4f),
+        new MaxPooling2D(2, 2, 2, "none"),
 
-//         new GlobalAveragePooling2D(),
-//         new Dense(128, new ReLU()),
-//         new Dropout(0.3f),
-//         new Dense(64, new ReLU()),    
-//         new Dropout(0.2f),        
-//         new Dense(10, new Softmax())
-//     };
+        new Conv2D(64, 3, 3, 1, "same", new ReLU(), 5e-4f),
+        new Conv2D(64, 3, 3, 1, "same", new ReLU(), 5e-4f),
+        new MaxPooling2D(2, 2, 2, "none"),
 
-//     // Creating Neural Network
-//     NeuralNet *nn = new NeuralNet(layers, loss);
+        new Conv2D(128, 3, 3, 1, "same", new ReLU(), 5e-4f),
+        new Conv2D(128, 3, 3, 1, "same", new ReLU(), 5e-4f),
+        new MaxPooling2D(2, 2, 2, "none"),
 
-//     // Creating Early Stop Object
-//     EarlyStop *stop = new EarlyStop(1, 1e-4, 5);
+        new Conv2D(256, 3, 3, 1, "same", new ReLU(), 5e-4f),
+        new Conv2D(256, 3, 3, 1, "same", new ReLU(), 5e-4f),
+        new MaxPooling2D(2, 2, 2, "none"),
 
-//     // Training Model
-//     ProgressMetric *metric = new ProgressAccuracy();
-//     nn->fit(
-//         xTrain, // Features
-//         yTrain, // Targets
-//         0.01,  // Learning rate
-//         0.01,    // Learning rate decay
-//         4,      // Number of epochs
-//         32,     // Batch Size
-//         *metric, // Progress metric
-//         xVal,  // Validation features
-//         yVal,   // Validation targets
-//         stop    // Early stop object
-//     );
+        new Conv2D(512, 3, 3, 1, "same", new ReLU(), 5e-4f),
+        new Conv2D(512, 3, 3, 1, "same", new ReLU(), 5e-4f),
+        new MaxPooling2D(2, 2, 2, "none"),
 
-//     // Saving Model
-//     Pipeline pipe;
-//     pipe.setData(data);
-//     pipe.setModel(nn);
-//     pipe.setImageTransformer2D(transformer);
-//     pipe.saveToBin("models/XrayCNNTrain");
+        new Flatten(),             
+        new Dense(128, new ReLU(), 5e-4f),
+        new Dropout(0.5f),
+        new Dense(64, new ReLU(), 5e-4f),
+        new Dropout(0.5f),
+        new Dense(2, new Softmax())               
+    };
 
-//     // Testing Model
-//     Tensor output = nn->predict(xTest);
-//     vector<float> predictions = TrainingUtils::getPredictions(output);
-//     float accuracy = 100.0f * TrainingUtils::getAccuracy(yTest, predictions);
-//     printf("\nTest Accuracy %.2f.\n", accuracy);
-// }
+    // Creating Neural Network
+    NeuralNet *nn = new NeuralNet(layers, loss);
+
+    // Creating Early Stop Object
+    EarlyStop *stop = new EarlyStop(8, 5e-4f, 5);
+
+    // Training Model
+    ProgressMetric *metric = new ProgressAccuracy();
+    nn->fit(
+        xTrain, // Features
+        yTrain, // Targets
+        0.005f,  // Learning rate
+        0.02f,    // Learning rate decay
+        50,      // Number of epochs
+        16,     // Batch Size
+        *metric, // Progress metric
+        xVal,  // Validation features
+        yVal,   // Validation targets
+        stop    // Early stop object
+    );
+
+    // Saving Model
+    Pipeline pipe;
+    pipe.setData(data);
+    pipe.setModel(nn);
+    pipe.setImageTransformer2D(transformer);
+    pipe.saveToBin("models/XrayCNNTrain");
+
+    // Testing Model
+    Tensor output = nn->predict(xTest);
+    vector<float> predictions = TrainingUtils::getPredictions(output);
+    float accuracy = 100.0f * TrainingUtils::getAccuracy(yTest, predictions);
+    printf("\nTest Accuracy %.2f.\n", accuracy);
+
+    // Delete pointers that don't belong to pipe
+    delete stop;
+    delete metric;
+}
--- src/mains/RegressMLPLoad.cpp
+++ src/mains/RegressMLPLoad.cpp
@@ -1,84 +1,84 @@
-#include <fstream>
-#include <iostream>
-#include <vector>
-#include <sstream>
-#include <cstdio>
-#include "core/model/NeuralNet.h"
-#include "core/data/TabularData.h"
-#include "core/activations/ReLU.h"
-#include "core/activations/Softmax.h"
-#include "core/activations/Linear.h"
-#include "core/losses/MSE.h"
-#include "core/losses/Loss.h"
-#include "core/losses/SoftmaxCrossEntropy.h"
-#include "utils/ConsoleUtils.h"
-#include "utils/Greyscale.h"
-#include "utils/Minmax.h"
-#include "utils/TrainingUtils.h"
-#include "core/layers/Layer.h"
-#include "core/layers/Dense.h"
-#include "core/metrics/ProgressAccuracy.h"
-#include "core/metrics/ProgressMAPE.h"
-#include "core/model/Pipeline.h"
-#include "utils/ImageTransform2D.h"
-#include "core/layers/Conv2D.h"
-#include "core/layers/Dropout.h"
-#include "core/layers/MaxPooling2D.h"
-#include "core/layers/Flatten.h"
-#include "core/data/ImageData2D.h"
-#include "core/gpu/GpuEngine.h"
+// #include <fstream>
+// #include <iostream>
+// #include <vector>
+// #include <sstream>
+// #include <cstdio>
+// #include "core/model/NeuralNet.h"
+// #include "core/data/TabularData.h"
+// #include "core/activations/ReLU.h"
+// #include "core/activations/Softmax.h"
+// #include "core/activations/Linear.h"
+// #include "core/losses/MSE.h"
+// #include "core/losses/Loss.h"
+// #include "core/losses/SoftmaxCrossEntropy.h"
+// #include "utils/ConsoleUtils.h"
+// #include "utils/Greyscale.h"
+// #include "utils/Minmax.h"
+// #include "utils/TrainingUtils.h"
+// #include "core/layers/Layer.h"
+// #include "core/layers/Dense.h"
+// #include "core/metrics/ProgressAccuracy.h"
+// #include "core/metrics/ProgressMAPE.h"
+// #include "core/model/Pipeline.h"
+// #include "utils/ImageTransform2D.h"
+// #include "core/layers/Conv2D.h"
+// #include "core/layers/Dropout.h"
+// #include "core/layers/MaxPooling2D.h"
+// #include "core/layers/Flatten.h"
+// #include "core/data/ImageData2D.h"
+// #include "core/gpu/GpuEngine.h"
 
-int main() {
+// int main() {
 
-    // Welcome Message
-    ConsoleUtils::printTitle();
+//     // Welcome Message
+//     ConsoleUtils::printTitle();
 
-    // Initialize Gpu if on Mac (Safe to call on non-Mac, it just won't do anything)
-    GpuEngine::init();
+//     // Initialize Gpu if on Mac (Safe to call on non-Mac, it just won't do anything)
+//     GpuEngine::init();
 
-    // Data Paths
-    const string trainPath = "DataFiles/California_Housing/housing_clean.csv";
-    const string testPath = "DataFiles/California_Housing/housing_clean.csv";
+//     // Data Paths
+//     const string trainPath = "DataFiles/California_Housing/housing_clean.csv";
+//     const string testPath = "DataFiles/California_Housing/housing_clean.csv";
 
-    // Must be in the same position as when the model was trained
-    const string targetColumn = "median_house_value";
+//     // Must be in the same position as when the model was trained
+//     const string targetColumn = "median_house_value";
 
-    // Loading Model
-    Pipeline pipe = Pipeline::loadFromBin("models/RegressHousingTrain");
-    NeuralNet *nn = pipe.getModel();
-    TabularData *data = dynamic_cast<TabularData*>(pipe.getData());
-    Scalar *featureScalar = pipe.getFeatureScalar();
-    Scalar *targetScalar = pipe.getTargetScalar();
+//     // Loading Model
+//     Pipeline pipe = Pipeline::loadFromBin("models/RegressHousingTrain");
+//     NeuralNet *nn = pipe.getModel();
+//     TabularData *data = dynamic_cast<TabularData*>(pipe.getData());
+//     Scalar *featureScalar = pipe.getFeatureScalar();
+//     Scalar *targetScalar = pipe.getTargetScalar();
     
-    // Data Reading
-    data->readTrain(trainPath, targetColumn);
-    data->readTest(testPath, targetColumn);
+//     // Data Reading
+//     data->readTrain(trainPath, targetColumn);
+//     data->readTest(testPath, targetColumn);
 
-    // Scaling Data
-    Tensor xTrain = featureScalar->transform(data->getTrainFeatures());
-    Tensor xTest = featureScalar->transform(data->getTestFeatures());
+//     // Scaling Data
+//     Tensor xTrain = featureScalar->transform(data->getTrainFeatures());
+//     Tensor xTest = featureScalar->transform(data->getTestFeatures());
 
-    vector<float> yTrain = targetScalar->transform(data->getTrainTargets());
-    vector<float> yTest = data->getTestTargets();
+//     vector<float> yTrain = targetScalar->transform(data->getTrainTargets());
+//     vector<float> yTest = data->getTestTargets();
 
-    // Training Model
-    ProgressMetric *metric = new ProgressMAPE();
-    nn->fit(
-        xTrain,  // Features
-        yTrain,  // Targets
-        0.0001,  // Learning rate
-        0.0001,  // Learning rate decay
-        2,       // Number of epochs
-        32,      // Batch Size
-        *metric  // Progress metric
-    );
+//     // Training Model
+//     ProgressMetric *metric = new ProgressMAPE();
+//     nn->fit(
+//         xTrain,  // Features
+//         yTrain,  // Targets
+//         0.0001,  // Learning rate
+//         0.0001,  // Learning rate decay
+//         2,       // Number of epochs
+//         32,      // Batch Size
+//         *metric  // Progress metric
+//     );
 
-    // Saving Model
-    pipe.saveToBin("models/RegressHousingLoad");
+//     // Saving Model
+//     pipe.saveToBin("models/RegressHousingLoad");
 
-    // Testing Model
-    Tensor output = nn->predict(xTest);
-    Tensor predictions = targetScalar->reverseTransform(output);
-    float rmse = TrainingUtils::getRMSE(predictions, yTest);
-    printf("\nTest RMSE: %.2f.\n", rmse);
-}
+//     // Testing Model
+//     Tensor output = nn->predict(xTest);
+//     Tensor predictions = targetScalar->reverseTransform(output);
+//     float rmse = TrainingUtils::getRMSE(predictions, yTest);
+//     printf("\nTest RMSE: %.2f.\n", rmse);
+// }
